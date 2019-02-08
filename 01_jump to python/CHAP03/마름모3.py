5451cfdfb49ceb6b7278a1607d80780507694936
xsky21 <studerande5@gmail.com>
2019-02-08 09:27:00 +0900

    무선공유기

---
+++ 01_jump to python/CHAP03/마름모3.py
@@ -0,0 +1,14 @@
+# coding: cp949
+while True:
+    num=int(input("홀수를 입력하세요(0<-종료): "))
+    empty=int(num/2)
+    point=num-2*empty
+    if num == 0 : break
+    elif num % 2 == 0 : continue
+    else:
+        print(" "+"-"*num+" ")
+        for empty_var in range(empty,-1):
+            print("|"+" "*empty+"*"*point
+            if for b in range(1, num, 2)kkkk
+                print("|"+" "*a+"*"*b+" "*a+"|")
+                break
