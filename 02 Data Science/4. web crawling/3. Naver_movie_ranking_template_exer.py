9748e569be228d7a6422a508ed361fd4a5f69df8
이세중 <studerande5@gmail.com>
2019-02-27 21:55:47 +0900

    ㅁㅇㄹ

--- 02 Data Science/4. web crawling/3. Naver_movie_ranking_template_exer.py
+++ 02 Data Science/4. web crawling/3. Naver_movie_ranking_template_exer.py
@@ -1,22 +1,21 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import re
 html = urllib.request.urlopen('http://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 soup = BeautifulSoup(html,'html.parser')
-tags = soup.find_all("a", href=re.compile('/movie/bi/mi/basic')) #영화 이름 찾기
-up_down = soup.find_all('td', attrs={'class':'range ac'})#순위 변동 태그 찾기
-index = soup.find_all('img',attrs={'width':'14'}) #순위 찾기
-up_down_k = soup.find_all("img",attrs={'class':'arrow'}) #순위 상승 여부
+tags = soup.find_all("div", attrs = {'class' : "tit3"}) #영화 이름 찾기
+up_down = soup.find_all('td', attrs={'class':'range ac'})#순위가 얼마나 바뀌었나
+index = soup.find_all('img',width="14", height="13") #순위 찾기
+up_down_image = soup.find_all("img", attrs = {'class':'arrow'}) #순위 상승 여부
 movie_list = []
 up_down_arrow =""
-for a in range(49):
-    if up_down_k[a].get("alt") == "na":
+for b in range(49):
+    if up_down_image[b].get("alt") == "na":
         up_down_arrow = " "
-    elif up_down_k[a].get("alt") == "up":
+    elif up_down_image[b].get("alt") == "up":
         up_down_arrow = "+"
-    elif up_down_k[a].get("alt") == "down":
+    elif up_down_image[b].get("alt") == "down":
         up_down_arrow = "-"
-    movie_list.append(index[a].get("alt") + '\t' + tags[a].get("title") + "\t" + up_down_arrow + up_down[a].text)
+    movie_list.append(index[b].get("alt") + '\t' + (tags[b].a).text + "\t" + up_down_arrow + up_down[b].text)
 print("\n".join(movie_list))
 movie_list = "\n".join(movie_list)
 with open("movie.csv",'w') as file:
--- 02 Data Science/4. web crawling/3. Naver_movie_ranking_template_exer_2.py
+++ 02 Data Science/4. web crawling/3. Naver_movie_ranking_template_exer_2.py
@@ -3,24 +3,13 @@ from bs4 import BeautifulSoup
 import re
 html = urllib.request.urlopen('http://movie.naver.com/movie/sdb/rank/rmovie.nhn')
 soup = BeautifulSoup(html,'html.parser')
-tags = soup.find_all("a", href=re.compile('/movie/bi/mi/basic')) #영화 이름 찾기
-m = re.compile('title="([\w:,]+)">')
-k = m.findall(str(tags)) #findall은 그냥 바로 group지정된걸 가져오나? k는 영화 이름 리스트
-up_down = soup.find_all('td', attrs={'class':'range ac'})#순위 변동 태그 찾기
-m = re.compile('>(\d)<')
-p = m.findall(str(up_down)) #p는 순위 변동 리스트
-index = soup.find_all('img',attrs={'width':'14'}) #순위 찾기
-m = re.compile('alt="([\d]{2})"')
-r = m.findall(str(index)) #r은 순위 리스트
-up_down_k = soup.find_all("img",attrs={'class':'arrow'}) #순위 상승 여부
-m = re.compile('alt="([\w]{2})"')
-t = m.findall(str(up_down_k)) #순위 상승 여부 리스트
-movie_list = []
-for a in range(45):
-    movie_list.append(r[a] + "\t" + k[a] + "\t" + t[a] + "\t" +p[a])
-
-print(movie_list)
-
-
-
+re_name = re.compile('href="/movie/bi/mi/basic.nhn[?]code=[0-9]{5,6}" title="(.+)"') #영화제목
+re_name_2 = re_name.findall(str(soup))
+re_ranking = re.compile('''alt="([0-9]+)" width="14" height="13"''')#영화 순위
+re_renking_2 = re_name.findall(str(soup))
+re_up_down = re.compile('''([0-9])</td>\n''')
+re_up_down_2 = re_up_down.findall(str(soup))#순위 변동
+re_arrow = re.compile('''" height="10" class="arrow"''')
+re_arrow_2 = re_arrow.findall(str(soup))
+print(len(re_arrow_2))
 
