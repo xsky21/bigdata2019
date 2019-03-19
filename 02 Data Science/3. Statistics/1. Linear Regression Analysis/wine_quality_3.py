27730bdd62e6540c101ea05e76fea5d20e6510f9
xsky21 <studerande5@gmail.com>
2019-03-19 09:29:25 +0900

    asdf

--- 02 Data Science/3. Statistics/1. Linear Regression Analysis/wine_quality_3.py
+++ 02 Data Science/3. Statistics/1. Linear Regression Analysis/wine_quality_3.py
@@ -3,13 +3,15 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Read the data set into a pandas DataFrame
 wine = pd.read_csv('winequality-both.csv', sep=',', header=0)
 wine.columns = wine.columns.str.replace(' ', '_')
 
+print("7.2.3 상관관계 분석")
+print("모든 변수 쌍 사이의 상관계수 구하기")
+print(wine.corr())
+
 def take_sample(data_frame, replace=False, n=200):
 	return data_frame.loc[np.random.choice(data_frame.index, replace=replace, size=n)]
-
 reds = wine.loc[wine['type']=='red', :]
 whites = wine.loc[wine['type']=='white', :]
 reds_sample = take_sample(wine.loc[wine['type']=='red', :])
@@ -24,7 +26,7 @@ wine_sample = pd.concat([reds_sample, whites_sample], ignore_index=True)
 print("\nprint: wine['in_sample']")
 print(wine['in_sample'])
 print("\nprint: pd.crosstab(wine.in_sample, wine.type, margins=True)")
-print(pd.crosstab(wine['in_sample'], wine['type'],margins=True))
+print(pd.crosstab(wine.in_sample, wine.type, margins=True))
 
 sns.set_style("dark")
 g = sns.pairplot(wine_sample, kind='reg', plot_kws={"ci": False, "x_jitter": 0.25, "y_jitter": 0.25}, \
